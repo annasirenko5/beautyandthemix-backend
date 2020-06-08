b5e480d3262d1200cfb796907c13e37df8a63b4d
DESKTOP-O3Q7QPL\hoang <hoangthuytien46@gmail.com>
2020-06-08 20:59:33 +0200

    update models and create controllers

---
+++ controllers/creditcard.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+var CreditCard = require('../models/creditcard');
+
+// Display CreditCard create form on GET.
+exports.CreditCard_create_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: CreditCard create GET');
+
+// Handle CreditCard create on POST.
+exports.CreditCard_create_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: CreditCard create POST');
+};
+
+// Display CreditCard delete form on GET.
+exports.CreditCard_delete_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: CreditCard delete GET');
+};
+
+// Handle CreditCard delete on POST.
+exports.CreditCard_delete_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: CreditCard delete POST');
+};
+
+// Display CreditCard update form on GET.
+exports.CreditCard_update_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: CreditCard update GET');
+};
+
+// Handle CreditCard update on POST.
+exports.CreditCard_update_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: CreditCard update POST');
+};
